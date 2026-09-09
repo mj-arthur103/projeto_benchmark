c4a1416f6463e32e129083b3b166fd0a8ab7945a
mj-arthur103 <arthurcarneiro522@gmail.com>
2026-09-09 09:12:04 -0300

    Add: Função Heapify e Heap Sort

--- metodos_ordenacao/selection_sort_v2.py
+++ metodos_ordenacao/selection_sort_v2.py
@@ -1,9 +1,37 @@
-def selection_sort(arr):
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    left = 2 * i + 1  # left child index
+    right = 2 * i + 2  # right child index
+
+    # If left child is larger than root
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    # If right child is larger than largest so far
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    # If largest is not root
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+
+        # Recursively heapify the affected sub-tree
+        heapify(arr, n, largest)
+def heap_sort(arr):
     n = len(arr)
-    for i in range(n):
-        min_index = i
-        for j in range(i + 1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    # Build a maxheap
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # One by one extract elements from heap
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
+
     return arr
+
+tamanho=input()
+valores=input()
+lista=[int(x) for x in valores.split()]
+print("Lista ordenada: ", heap_sort(lista))
